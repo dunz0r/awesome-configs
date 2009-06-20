35e1bbcc975bcc0ad72cf856daef879cbbb413c1
Gabriel Fornaeus <gfornaeus@gmail.com>
2009-06-20 23:27:54 +0200

    reverted back to konius theme

--- theme.lua
+++ theme.lua
@@ -8,19 +8,17 @@ theme.font = "Sans 7"
 theme.tasklist_font = "Sans 7"
 theme.titlebar_font = "Sans 7"
 
-theme.bg_focus      = "#d0d0d0a0"
-theme.bg_normal     = "#7f7f7f00"
+theme.bg_normal     = "#171717"
+theme.bg_focus      = "#524E41"
 theme.bg_urgent     = "#171717"
 
-theme.fg_normal     = "#000000"
-theme.fg_focus      = "#404040"
+theme.fg_normal     = "#999999"
+theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffc500"
 
-theme.bg_widget     = "#ffffff"
-theme.fg_widget     = "#c0c0c0"
 theme.border_width  = 1
-theme.border_normal = "#505050"
-theme.border_focus  = "#c0c0c0"
+theme.border_normal = "#151515"
+theme.border_focus  = "#524E41"
 theme.border_marked = "#91231c"
 
 theme.widget_label = "#333333"
@@ -36,17 +34,17 @@ theme.menu_height   = 15
 theme.menu_width    = 100
 
 
-theme.wallpaper_cmd = { "feh --bg-tile /home/dunz0r/.wallpaper" }
+theme.wallpaper_cmd = { "xsetroot -solid '#524E41'" }
 
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairh.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairv.png"
-theme.layout_floating = "/usr/share/awesome/themes/default/layouts/floating.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifier.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/max.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottom.png"
-theme.layout_tileleft = "/usr/share/awesome/themes/default/layouts/tileleft.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tile.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletop.png"
+theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
+theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
+theme.layout_floating = "/usr/share/awesome/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
+theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
+theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
+theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
+theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
