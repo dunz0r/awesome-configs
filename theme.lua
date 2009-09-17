5a80cceb816443e44df112c9ffff0c1ff664b552
dunz0r <gfornaeus@gmail.com>
2009-09-17 20:22:23 +0200

    Now with Zenburn colors!

--- theme.lua
+++ theme.lua
@@ -1,55 +1,142 @@
----------------------------
--- my awesome theme -------
----------------------------
-
-theme = {}
-
-theme.font = "fixed 9"
-theme.tasklist_font = "fixed 9"
-theme.titlebar_font = "fixed 9"
-
-theme.bg_normal     = "#333333"
-theme.bg_focus      = "#1793d1"
-theme.bg_urgent     = "#333333"
-
-theme.fg_normal     = "#999999"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffc500"
-
-theme.border_width  = 1
-theme.border_normal = "#333333"
-theme.border_focus  = "#1793d1"
-theme.border_marked = "#91231c"
-
-theme.widget_label = "#333333"
-theme.widget_value = "#555555"
-
-theme.help_font = "Monospace 6.5"
-theme.help_highlight = theme.fg_urgent
-
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
-theme.menu_height   = 15
-theme.menu_width    = 100
+-------------------------------
+--  "Zenburn" awesome theme  --
+--    By Adrian C. (anrxc)   --
+-------------------------------
 
 
---theme.wallpaper_cmd = { "feh --bg-tile /home/dunz0r/.wallpaper" }
---theme.wallpaper_cmd = { "xsetroot -solid '#524E41'" }
-theme.wallpaper_cmd = { "xsetroot -solid '#333333'" }
+-- {{{ Main
+theme   = {}
+confdir = awful.util.getdir("config")
+theme.wallpaper_cmd = { "/usr/bin/nitrogen --restore" }
+--theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/zenburn/zenburn-background.png" }
+-- }}}
 
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
 
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+-- {{{ Styles
+theme.font      = "fixed 9"
+
+-- {{{ Colors
+theme.fg_normal = "#DCDCCC"
+theme.fg_focus  = "#F0DFAF"
+theme.fg_urgent = "#CC9393"
+theme.bg_normal = "#3F3F3F"
+theme.bg_focus  = "#1E2320"
+theme.bg_urgent = "#3F3F3F"
+-- }}}
+
+-- {{{ Borders
+theme.border_width  = "1"
+theme.border_normal = "#3F3F3F"
+theme.border_focus  = "#6F6F6F"
+theme.border_marked = "#CC9393"
+-- }}}
+
+-- {{{ Titlebars
+theme.titlebar_bg_focus  = "#3F3F3F"
+theme.titlebar_bg_normal = "#3F3F3F"
+-- theme.titlebar_[normal|focus]
+-- }}}
+
+-- {{{ Widgets
+theme.fg_widget        = "#AECF96"
+theme.fg_center_widget = "#88A175"
+theme.fg_end_widget    = "#FF5656"
+theme.fg_off_widget    = "#494B4F"
+theme.fg_netup_widget  = "#7F9F7F"
+theme.fg_netdn_widget  = "#CC9393"
+theme.bg_widget        = "#3F3F3F"
+theme.border_widget    = "#3F3F3F"
+-- }}}
+
+-- {{{ Mouse finder
+theme.mouse_finder_color = "#CC9393"
+-- theme.mouse_finder_[timeout|animate_timeout|radius|factor]
+-- }}}
+
+-- {{{ Tooltips
+-- theme.tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- }}}
+
+-- {{{ Taglist and Tasklist
+-- theme.[taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- }}}
+
+-- {{{ Menu
+-- theme.menu_[height|width]
+-- theme.menu_[bg|fg]_[normal|focus]
+-- theme.menu_[border_color|border_width]
+-- }}}
+-- }}}
+
+
+-- {{{ Icons
+--
+-- {{{ Taglist icons
+theme.taglist_squares_sel   = confdir .. "/icons/taglist/squarefw.png"
+theme.taglist_squares_unsel = confdir .. "/icons/taglist/squarew.png"
+--theme.taglist_squares_resize = "false"
+-- }}}
+
+-- {{{ Misc icons
+--theme.awesome_icon           = confdir .. "/icons/awesome.png"
+--theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
+--theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+-- }}}
+
+-- {{{ Layout icons
+theme.layout_tile       = confdir .. "/icons/layouts/tilew.png"
+theme.layout_tileleft   = confdir .. "/icons/layouts/tileleftw.png"
+theme.layout_tilebottom = confdir .. "/icons/layouts/tilebottomw.png"
+theme.layout_tiletop    = confdir .. "/icons/layouts/tiletopw.png"
+theme.layout_fairv      = confdir .. "/icons/layouts/fairvw.png"
+theme.layout_fairh      = confdir .. "/icons/layouts/fairhw.png"
+theme.layout_spiral     = confdir .. "/icons/layouts/spiralw.png"
+theme.layout_dwindle    = confdir .. "/icons/layouts/dwindlew.png"
+theme.layout_max        = confdir .. "/icons/layouts/maxw.png"
+theme.layout_fullscreen = confdir .. "/icons/layouts/fullscreenw.png"
+theme.layout_magnifier  = confdir .. "/icons/layouts/magnifierw.png"
+theme.layout_floating   = confdir .. "/icons/layouts/floatingw.png"
+-- }}}
+
+-- {{{ Widget icons
+theme.widget_cpu    = confdir .. "/icons/cpu.png"
+theme.widget_bat    = confdir .. "/icons/bat.png"
+theme.widget_mem    = confdir .. "/icons/mem.png"
+theme.widget_fs     = confdir .. "/icons/disk.png"
+theme.widget_net    = confdir .. "/icons/down.png"
+theme.widget_netup  = confdir .. "/icons/up.png"
+theme.widget_mail   = confdir .. "/icons/mail.png"
+theme.widget_vol    = confdir .. "/icons/vol.png"
+theme.widget_org    = confdir .. "/icons/cal.png"
+theme.widget_date   = confdir .. "/icons/time.png"
+theme.widget_crypto = confdir .. "/icons/crypto.png"
+-- }}}
+
+-- {{{ Titlebar icons
+theme.titlebar_close_button_focus  = confdir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = confdir .. "/icons/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active    = confdir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active   = confdir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = confdir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = confdir .. "/icons/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active    = confdir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active   = confdir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = confdir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = confdir .. "/icons/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active    = confdir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active   = confdir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = confdir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = confdir .. "/icons/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active    = confdir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = confdir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = confdir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = confdir .. "/icons/titlebar/maximized_normal_inactive.png"
+-- }}}
+-- }}}
+
 
 return theme
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
